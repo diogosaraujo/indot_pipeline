437e06d75151357c74cb49f36e9a317057b02e51
diogosaraujo <daraujo@nd.edu>
2026-05-04 12:00:08 -0400

    Added AWS CLI Command to create EC2 instance

--- launch-ec2.bat
+++ launch-ec2.bat
@@ -19,25 +19,33 @@ echo Finding latest Ubuntu 24.04 LTS AMI...
 for /f %%i in ('aws ec2 describe-images --region %REGION% --owners 099720109477 --filters "Name=name,Values=ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*" "Name=state,Values=available" --query "sort_by(Images, &CreationDate)[-1].ImageId" --output text') do set AMI_ID=%%i
 echo AMI ID: %AMI_ID%
 
-:: Step 3: Create Security Group
-echo Creating security group...
-for /f %%i in ('aws ec2 create-security-group --group-name %SG_NAME% --description "INDOT Pipeline - SSH from my IP only" --region %REGION% --query "GroupId" --output text') do set SG_ID=%%i
-echo Security Group ID: %SG_ID%
+:: Step 3: Check if security group already exists, reuse it if so
+echo Checking for existing security group...
+for /f %%i in ('aws ec2 describe-security-groups --region %REGION% --filters "Name=group-name,Values=%SG_NAME%" --query "SecurityGroups[0].GroupId" --output text 2^>nul') do set SG_ID=%%i
 
-:: Step 4: Add SSH inbound rule from your IP only
-echo Adding SSH inbound rule...
-aws ec2 authorize-security-group-ingress --group-id %SG_ID% --protocol tcp --port 22 --cidr %MY_IP%/32 --region %REGION%
+if "%SG_ID%"=="None" set SG_ID=
+if "%SG_ID%"=="" (
+    echo Security group not found, creating it...
+    for /f %%i in ('aws ec2 create-security-group --group-name %SG_NAME% --description "INDOT Pipeline - SSH from my IP only" --region %REGION% --query "GroupId" --output text') do set SG_ID=%%i
+    echo Created Security Group ID: %SG_ID%
 
-:: Step 5: Launch the EC2 instance
+    :: Add SSH inbound rule only when creating a new group
+    echo Adding SSH inbound rule...
+    aws ec2 authorize-security-group-ingress --group-id %SG_ID% --protocol tcp --port 22 --cidr %MY_IP%/32 --region %REGION%
+) else (
+    echo Reusing existing Security Group ID: %SG_ID%
+)
+
+:: Step 4: Launch the EC2 instance
 echo Launching EC2 instance...
-for /f %%i in ('aws ec2 run-instances --region %REGION% --image-id %AMI_ID% --instance-type %INSTANCE_TYPE% --key-name %KEY_NAME% --security-group-ids %SG_ID% --iam-instance-profile Name=%IAM_ROLE% --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":200,\"VolumeType\":\"gp3\",\"DeleteOnTermination\":true}}]" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=indot-pipeline}]" --query "Instances[0].InstanceId" --output text') do set INSTANCE_ID=%%i
+for /f %%i in ('aws ec2 run-instances --region %REGION% --image-id %AMI_ID% --instance-type %INSTANCE_TYPE% --key-name %KEY_NAME% --security-group-ids %SG_ID% --iam-instance-profile "Name=%IAM_ROLE%" --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":200,\"VolumeType\":\"gp3\",\"DeleteOnTermination\":true}}]" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=indot-pipeline}]" --query "Instances[0].InstanceId" --output text') do set INSTANCE_ID=%%i
 echo Instance launched: %INSTANCE_ID%
 
-:: Step 6: Wait for instance to be running
+:: Step 5: Wait for instance to be running
 echo Waiting for instance to reach running state...
 aws ec2 wait instance-running --instance-ids %INSTANCE_ID% --region %REGION%
 
-:: Step 7: Get public IP
+:: Step 6: Get public IP
 for /f %%i in ('aws ec2 describe-instances --instance-ids %INSTANCE_ID% --region %REGION% --query "Reservations[0].Instances[0].PublicIpAddress" --output text') do set PUBLIC_IP=%%i
 
 echo.
